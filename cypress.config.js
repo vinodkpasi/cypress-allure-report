4e63688e4c5b64458de70b85869e81b89a2210ac
vinodkpasi <vinodkpasi@gmail.com>
2023-12-05 10:01:33 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,14 @@
 const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 const execSync = require('child_process').execSync;
-
+const fs = require('fs');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       on('before:run', (details) => {
-        execSync("if exist allure-report rmdir allure-report /s /q");
-        execSync("if exist allure-results rmdir allure-results /s /q");
+        fs.rmSync("allure-report", { recursive: true, force: true });
+        fs.rmSync("allure-results", { recursive: true, force: true });
       })
       on('after:run', (details) => {
         execSync('npx allure generate allure-results --single-file --clean -o allure-report allure-report && npx allure open allure-report');
